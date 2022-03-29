f055e2e0ca48dfa115e44eaa25851e8534fdc724
Kaveh Farsi Nezhad <98241768+kavehfarsi@users.noreply.github.com>
2022-03-29 20:39:55 +0430

    first upload

---
+++ menu.js
@@ -0,0 +1,25 @@
+function red()
+{
+    var u = document.getElementById("centerul");
+    u.style.backgroundColor = "red";
+}
+function blue()
+{
+    var u = document.getElementById("centerul");
+    u.style.backgroundColor = "darkblue";
+}
+function green()
+{
+    var u = document.getElementById("centerul");
+    u.style.backgroundColor = "green";
+}
+function black()
+{
+    var u = document.getElementById("centerul");
+    u.style.backgroundColor = "black";
+}
+function changetext()
+{
+    document.getElementById("head").innerHTML= "Try to Change Menu Background"
+    document.getElementById("head").style.color = "darkgreen"
+}
